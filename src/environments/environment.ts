86ee782200b6c9af941432c809fb2c0e3bd04018
Daniel Saluceste <daniel.saluceste2@gmail.com>
2022-12-01 07:27:25 -0300

    Nava env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,10 +4,10 @@
 
 export const environment = {
   production: false,
-  // apiUrl: "https://apiprojeto.onecoding.com.br",
+   apiUrl: "https://apiprojeto.onecoding.com.br",
   // apiUrl: "http://45.33.72.90:3000",
   // apiUrl: "http://localhost:3000",
-   apiUrl: "http://192.168.0.106:3000",
+  // apiUrl: "http://192.168.0.106:3000",
 };
 
 /*
